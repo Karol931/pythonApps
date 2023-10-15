e732d561f7343c0de701145fa2df0c7d078030ce
Karol931 <kahder6@gmail.com>
2023-10-15 14:36:00 +0200

    Added paly again button

--- pythonApps/hang_man/views.py
+++ pythonApps/hang_man/views.py
@@ -6,151 +6,6 @@ from utils import hangman
 from hang_man.models import Game, Letters
 # Create your views here.
 
-
-# def index(request):
-#     template = "hang_man/index.html"
-#     if request.method == "GET":
-#         Game.objects.all().delete()
-#         Letters.objects.all().delete()
-#         original_word, guessed_word, life_count =  hangman.start()
-#         form = LetterFrom()
-#         game = Game(lifes=life_count, original_word = original_word, guessed_word = guessed_word)
-#         game.save()
-#         context = {
-#             'lifes' : life_count,
-#             'word' : guessed_word,
-#             'form' : form,
-#             'letters' : [],
-#             'err' : '',
-#             'end' : ''
-#         } 
-#         return render(request, template, context)
-    
-#     if request.method == "POST":
-#         form = LetterFrom(request.POST)
-#         game = Game.objects.get()
-
-#         if form.is_valid():
-#             letter = form.cleaned_data["letter"].lower()
-#             form = LetterFrom()
-#             guessed_letters = Letters.objects.all()
-            
-#             if letter in [l.letters for l in guessed_letters]:
-#                 # When u guess letter again
-#                 context = {
-#                     'lifes' : game.lifes,
-#                     'word' : game.guessed_word,
-#                     'form' : form,
-#                     'letters' : [l.letters for l in guessed_letters],
-#                     'err' : f'Can\'t guess {letter} again',
-#                     'end' : ''
-#                 }
-#                 print('1')
-#                 return render(request, template, context)
-            
-#             else:
-#                 idx = hangman.letter_check(letter, game.original_word)
-#                 print(idx)
-                
-#                 if not idx:
-#                     # When u guess wrong letter
-#                     letters = Letters(letters=letter)
-#                     game.lifes -= 1
-#                     game.save()
-#                     letters.save()
-#                     guessed_letters = Letters.objects.all()
-#                     context = {
-#                         'lifes' : game.lifes,
-#                         'word' : game.guessed_word,
-#                         'form' : form,
-#                         'letters' : [l.letters for l in guessed_letters],
-#                         'err' : '',
-#                         'end' : ''
-#                     }
-                    
-#                     if game.lifes == 0:
-#                         # When u lose
-#                         context = {
-#                             'lifes' : game.lifes,
-#                             'word' : game.guessed_word,
-#                             'form' : form,
-#                             'letters' : [l.letters for l in guessed_letters],
-#                             'err' : '',
-#                             'end' : 'You lost, refresh to play again!'
-#                         }
-#                         print('1')
-#                         return render(request, template, context)
-#                     elif game.original_word == game.guessed_word:
-#                         context = {
-#                             'lifes' : game.lifes,
-#                             'word' : game.guessed_word,
-#                             'form' : form,
-#                             'letters' : [l.letters for l in guessed_letters],
-#                             'err' : '',
-#                             'end' : 'You won congratulations, refresh to play again!'
-#                         }
-#                         print('1')
-#                         return render(request, template, context)
-
-#                     return render(request, template, context)
-                
-#                 else:
-#                     # When u guess right letter
-#                     game.guessed_word = hangman.update_word(indexes=idx, letter=letter,guessed_word=game.guessed_word)
-
-#                     letters = Letters(letters=letter)
-
-#                     game.save()
-#                     letters.save()
-#                     guessed_letters = Letters.objects.all()
-#                     context = {
-#                         'lifes' : game.lifes,
-#                         'word' : game.guessed_word,
-#                         'form' : form,
-#                         'letters' : [l.letters for l in guessed_letters],
-#                         'err' : '',
-#                         'end' : ''
-#                     }
-#                     if game.lifes == 0:
-#                         # When u lose
-#                         context = {
-#                             'lifes' : game.lifes,
-#                             'word' : game.guessed_word,
-#                             'form' : form,
-#                             'letters' : [l.letters for l in guessed_letters],
-#                             'err' : '',
-#                             'end' : 'You lost, refresh to play again!'
-#                         }
-#                         print('1')
-#                         return render(request, template, context)
-#                     elif game.original_word == game.guessed_word:
-#                         context = {
-#                             'lifes' : game.lifes,
-#                             'word' : game.guessed_word,
-#                             'form' : form,
-#                             'letters' : [l.letters for l in guessed_letters],
-#                             'err' : '',
-#                             'end' : 'You won congratulations, refresh to play again!'
-#                         }
-#                         print('1')
-#                         return render(request, template, context)
-                    
-#                     return render(request, template, context)
-       
-#         else:
-#             guessed_letters = Letters.objects.all()
-#             context = {
-#                     'lifes' : game.lifes,
-#                     'word' : game.guessed_word,
-#                     'form' : form,
-#                     'letters' : [l.letters for l in guessed_letters],
-#                     'err' : '',
-#                     'end' : ''
-#                 }
-#             print('4')
-#             return render(request, template, context)
-
-
 def process_form(request, form):
     game = Game.objects.get()
 
@@ -211,9 +66,9 @@ def index(request):
             game = Game.objects.get()
 
             if game.lifes == 0:
-                end_message = 'You lost, refresh to play again!'
+                end_message = 'You lost.'
             elif game.original_word == game.guessed_word:
-                end_message = 'You won congratulations, refresh to play again'
+                end_message = 'You won congratulations!'
             else:
                 end_message = ''
 
--- pythonApps/utils/hangman.py
+++ pythonApps/utils/hangman.py
@@ -22,23 +22,10 @@ def start():
     life_count = 3
 
     original_word = get_word().lower()
-    print(original_word)
 
     guessed_word = ('_' * len(original_word)).strip()
     
-    # print("Your word is, guess a letter:\n")
-
     return original_word, guessed_word, life_count
-
-# Checks if input is valid
-def input_check(letter, original_word, guessed_word, lifes):
-
-    if len(letter) != 1:
-        print("Type only one letter")
-        game(original_word, guessed_word, lifes)
-    if not letter.isalpha():
-        print("It's not a letter")
-        game(original_word, guessed_word, lifes)
 
 # Checks if letter is in the word
 def letter_check(letter, original_word):
@@ -57,29 +44,3 @@ def update_word(indexes, letter, guessed_word):
     guessed_word = "".join(s)
 
     return guessed_word
-
-
-def game(original_word, guessed_word, lifes, letter):
-    if lifes == 0:
-        exit(f"Game Over, word was {original_word}")
-    
-    print()
-    print('\u2764\uFE0F \t' * lifes)
-    print(guessed_word)
-    
-    if '_' not in guessed_word:
-        exit("Congratulations you won!!")
-    
-    input_check(letter, original_word, guessed_word, lifes)
-    
-    indexes = letter_check(letter, original_word)
-    
-    if not indexes:
-        lifes -= 1
-        if lifes != 0:
-            print("Wrong, guess again")
-        game(original_word, guessed_word, lifes)
-    
-    guessed_word = update_word(indexes, letter, guessed_word)
-
-    return original_word, guessed_word, lifes
